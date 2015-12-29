bb521757e97b931233a3897d5eb5184b050aa198
ukimiawz <melisa.cecilia.mail@gmail.com>
2015-12-29 11:08:22 -0800

    add client for service consume

---
+++ XmlRpcClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlRpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathService.MathsClient obj = new XmlRpcClient.MathService.MathsClient();
+            MathService.Math mathobj = new XmlRpcClient.MathService.Math();
+            mathobj.Number1 = 10;
+            mathobj.Number2 = 5;
+            Console.WriteLine("Addition continues..");
+            Console.WriteLine(obj.Addition(mathobj));
+            Console.WriteLine("Subtraction continues..");
+            Console.WriteLine(obj.Subtraction(mathobj));
+            Console.ReadLine();
+        }
+    }
+}
